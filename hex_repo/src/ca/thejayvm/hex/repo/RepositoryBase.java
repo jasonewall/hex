6994a4fd3517dd4c66903fdbccbe8ddeea509033
Jason Wall <thejayvm@users.noreply.github.com>
2014-10-29 10:31:41 -0600

    I need my space

--- hex_repo/src/ca/thejayvm/hex/repo/RepositoryBase.java
+++ hex_repo/src/ca/thejayvm/hex/repo/RepositoryBase.java
@@ -27,7 +27,7 @@ public abstract class RepositoryBase<T> implements Queryable<T> {
 
     public String toSql(Query<T> query) throws InvalidAstException {
         SqlQuery result = new SqlQuery(metadata);
-        result.from(new Node[] { new Variable(getTableName())});
+        result.from(new Node[] { new Variable(getTableName()) });
         Predicate<T> predicate = query.getPredicate();
         if(predicate != null && predicate instanceof Node) {
             result.where(((Node) predicate).toTree());
