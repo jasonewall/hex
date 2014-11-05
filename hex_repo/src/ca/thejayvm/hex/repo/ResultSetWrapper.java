fdce1355bb0f20877def872dd99466125313a659
Jason Wall <javajo@gmail.com>
2014-11-04 23:02:27 -0700

    stop delegating to unneccessary lambdas for getting return values

--- hex_repo/src/ca/thejayvm/hex/repo/ResultSetWrapper.java
+++ hex_repo/src/ca/thejayvm/hex/repo/ResultSetWrapper.java
@@ -15,43 +15,39 @@ public class ResultSetWrapper implements AutoCloseable {
         this.rs = rs;
     }
 
-    private static SQLFunction<ResultSet,Object> findGetter(ResultSetMetaData meta, int columnIndex) throws SQLException {
-        switch(meta.getColumnType(columnIndex)) {
-            case Types.BIGINT: return (rs) -> rs.getLong(columnIndex);
-            case Types.INTEGER: return (rs) -> rs.getInt(columnIndex);
-            case Types.BOOLEAN: return (rs) -> rs.getBoolean(columnIndex);
-            case Types.CHAR:
-            case Types.LONGVARCHAR:
-            case Types.VARCHAR:
-                return (rs) -> rs.getString(columnIndex);
-            case Types.DATE: return (rs) -> rs.getDate(columnIndex);
-            case Types.DECIMAL:
-            case Types.DOUBLE:
-            case Types.FLOAT:
-            case Types.NUMERIC:
-                return (rs) -> rs.getDouble(columnIndex);
-            case Types.LONGNVARCHAR:
-            case Types.NCHAR:
-            case Types.NVARCHAR:
-                return (rs) -> rs.getNString(columnIndex);
-            case Types.NULL: return (rs) -> null;
-            case Types.SMALLINT: return (rs) -> rs.getShort(columnIndex);
-            case Types.TIME:
-            case Types.TIME_WITH_TIMEZONE:
-                return (rs) -> rs.getTime(columnIndex);
-            case Types.TIMESTAMP:
-            case Types.TIMESTAMP_WITH_TIMEZONE:
-                return (rs) -> rs.getTimestamp(columnIndex);
-        }
-        throw new SQLException("Unhandled column type found while mapping data to instance.");
-    }
-
     public boolean next() throws SQLException {
         return rs.next();
     }
 
-    public Object getValue(int colIndex) throws SQLException {
-        return findGetter(metaData, colIndex).apply(rs);
+    public Object getValue(int columnIndex) throws SQLException {
+        switch(getMetaData().getColumnType(columnIndex)) {
+            case Types.BIGINT: return rs.getLong(columnIndex);
+            case Types.INTEGER: return rs.getInt(columnIndex);
+            case Types.BOOLEAN: return rs.getBoolean(columnIndex);
+            case Types.CHAR:
+            case Types.LONGVARCHAR:
+            case Types.VARCHAR:
+                return rs.getString(columnIndex);
+            case Types.DATE: return rs.getDate(columnIndex);
+            case Types.DECIMAL:
+            case Types.DOUBLE:
+            case Types.FLOAT:
+            case Types.NUMERIC:
+                return rs.getDouble(columnIndex);
+            case Types.LONGNVARCHAR:
+            case Types.NCHAR:
+            case Types.NVARCHAR:
+                return rs.getNString(columnIndex);
+            case Types.NULL: return null;
+            case Types.SMALLINT: return rs.getShort(columnIndex);
+            case Types.TIME:
+            case Types.TIME_WITH_TIMEZONE:
+                return rs.getTime(columnIndex);
+            case Types.TIMESTAMP:
+            case Types.TIMESTAMP_WITH_TIMEZONE:
+                return rs.getTimestamp(columnIndex);
+        }
+        throw new SQLException("Unhandled column type found while mapping data to instance.");
     }
 
     private ResultSetMetaData metaData;
