dcc0a2d8aface6ae7bb5d4a33c1e41895145014c
Jason Wall <javajo@gmail.com>
2014-11-27 22:15:17 -0700

    put example app back to original path format

--- example_application/adapters/controllers/PeopleController.java
+++ example_application/adapters/controllers/PeopleController.java
@@ -18,7 +18,12 @@ public class PeopleController extends Controller {
         view.put("person", person);
     }
 
-    public void home() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public void readme() {
+        renderText("Hex README");
+        renderText("Hi. Welcome to Hex.");
+    }
+
+    public void home() {
         view.put("message", "Hello World!");
         renderPage("layout.jsp");
     }
