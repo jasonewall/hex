e8229b20c622d68b9787db6a676e83afc5714bc6
Jason Wall <javajo@gmail.com>
2014-11-29 16:20:04 -0700

    create repo test suite

---
+++ hex_repo/test/hex/repo/RepoTests.java
@@ -0,0 +1,10 @@
+package hex.repo;
+
+import hex.repo.sql.SqlQueryTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SqlQueryTest.class})
+public class RepoTests {
+}
