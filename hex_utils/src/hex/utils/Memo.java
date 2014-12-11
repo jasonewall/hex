567ef92bf65fdf8ec40f05330ce1c774d03472c1
Jason Wall <javajo@gmail.com>
2014-12-10 23:00:51 -0700

    get ifPresent working on the params interface

---
+++ hex_utils/src/hex/utils/Memo.java
@@ -0,0 +1,28 @@
+package hex.utils;
+
+import java.util.function.UnaryOperator;
+
+/**
+ * Created by jason on 14-12-10.
+ */
+public class Memo<T> {
+    private final T t;
+
+    public static <T> Memo<T> of(T t) {
+        return new Memo<>(t);
+    }
+
+    private Memo(T t) {
+        this.t = t;
+    }
+
+    public Memo<T> andThen(UnaryOperator<T> op) {
+        T t = op.apply(this.t);
+        if(t == null) return this;
+        return new Memo<>(t);
+    }
+
+    public T finish() {
+        return t;
+    }
+}
