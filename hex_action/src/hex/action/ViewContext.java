bb5fd1fb1e9d6f25b3726d7333b0b36106d605be
Jason Wall <javajo@gmail.com>
2014-12-12 01:00:47 -0700

    view context should implements coercionmap

--- hex_action/src/hex/action/ViewContext.java
+++ hex_action/src/hex/action/ViewContext.java
@@ -1,16 +1,11 @@
 package hex.action;
 
+import hex.utils.CoercionMap;
+
 import java.util.HashMap;
 
 /**
  * Created by jason on 14-11-15.
  */
-public class ViewContext extends HashMap<String,Object> {
-    public boolean getBoolean(String attribute) {
-        return (boolean)get(attribute);
-    }
-
-    public int getInt(String attribute) {
-        return (int)get(attribute);
-    }
+public class ViewContext extends HashMap<String,Object> implements CoercionMap {
 }
