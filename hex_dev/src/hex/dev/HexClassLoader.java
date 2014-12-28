168087d56f3802b8e98787e253c7023ddba7ea6b
Jason Wall <javajo@gmail.com>
2014-12-28 16:57:52 -0700

    axe java doc because

--- hex_dev/src/hex/dev/HexClassLoader.java
+++ hex_dev/src/hex/dev/HexClassLoader.java
@@ -16,36 +16,11 @@ public class HexClassLoader extends URLClassLoader {
         this.compiler = compiler;
     }
 
-    /**
-     * Returns an input stream for reading the specified resource.
-     * If this loader is closed, then any resources opened by this method
-     * will be closed.
-     * <p>
-     * <p> The search order is described in the documentation for {@link
-     * #getResource(String)}.  </p>
-     *
-     * @param name The resource name
-     * @return An input stream for reading the resource, or {@code null}
-     * if the resource could not be found
-     * @since 1.7
-     */
     @Override
     public InputStream getResourceAsStream(String name) {
         return super.getResourceAsStream(name);
     }
 
-    /**
-     * Loads the class with the specified <a href="#name">binary name</a>.
-     * This method searches for classes in the same manner as the {@link
-     * #loadClass(String, boolean)} method.  It is invoked by the Java virtual
-     * machine to resolve class references.  Invoking this method is equivalent
-     * to invoking {@link #loadClass(String, boolean) <tt>loadClass(name,
-     * false)</tt>}.
-     *
-     * @param name The <a href="#name">binary name</a> of the class
-     * @return The resulting <tt>Class</tt> object
-     * @throws ClassNotFoundException If the class was not found
-     */
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
         // java.lang.String -> java/lang/String
